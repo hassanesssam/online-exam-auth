046cc77b62e10db7d496a63fc86d2240610250cb
hassanesssam <hassanessam909@gmail.com>
2024-12-02 13:02:20 +0200

    Updated middleware

---
+++ src/middleware.ts
@@ -0,0 +1,14 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export default function middleware(request : NextRequest){
+    const token = request.cookies.get("next-auth.session-token")
+    if(!token){
+     return NextResponse.rewrite(new URL("/login" , request.url))
+    }
+
+    return NextResponse.next();
+}
+
+export const config = {
+    matcher: [ "/"], 
+  };
